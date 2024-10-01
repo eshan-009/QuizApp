9e513de6c9610820e754d7cd6b585644772d72ab
eshan-009 <eshansharma009@gmail.com>
2024-10-01 19:45:31 +0530

    first commit

---
+++ backend/src/models/topics.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const topicSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    questions : [
+        {
+            type : mongoose.Types.ObjectId,
+            ref : "QUESTION"
+        }
+    ],
+    imageUrl:{
+        type:String
+    },
+    createdBy :  {
+        type : mongoose.Types.ObjectId,
+        ref : "USER"
+    }
+})
+
+module.exports =   mongoose.model("TOPIC",topicSchema);
